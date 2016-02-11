f899110340df9a0d3a18fd3ee05ef13494c900a0
Neuman Vong <neuman.vong@gmail.com>
2016-02-10 22:38:12 -0800

    test discard

--- test/asyncqueue.test.js
+++ test/asyncqueue.test.js
@@ -1,6 +1,8 @@
 var assert = require("assert");
 var asyncqueue = require("../src/asyncqueue");
 
+require("../src/promise").patch(Promise.prototype);
+
 test("offers then polls", function(done) {
   var q = asyncqueue();
   q.offer(1);
@@ -60,6 +62,31 @@ test("first failure", function(done) {
   );
 });
 
+test("fail with discard", function(done) {
+  var q = asyncqueue();
+  q.offer(1);
+  q.offer(2);
+  q.fail(new Error(), true);
+  var p = q.poll();
+  p.lift().then(function(value) {
+    assert.ok(value.rejected);
+  }).then(done, done);
+});
+
+test("fail without discard", function(done) {
+  var q = asyncqueue();
+  var exc = new Error("boo");
+  q.offer(1);
+  q.offer(2);
+  q.fail(exc, false);
+  var all = Promise.all([q.poll().lift(), q.poll().lift(), q.poll().lift()]);
+  all.then(function(values) {
+    assert.deepEqual({ resolved: 1 }, values[0]);
+    assert.deepEqual({ resolved: 2 }, values[1]);
+    assert.deepEqual({ rejected: exc }, values[2]);
+  }).then(done, done);
+});
+
 function test(name, go) {
   var done = false;
   var error;
@@ -73,10 +100,10 @@ function test(name, go) {
     try {
       assert.ok(done);
       assert.equal(undefined, error);
-      console.log("ok:\t", name);
+      console.log("[ok]", "\t", name);
     } catch(e) {
-      console.log("failed:\t", name);
-      if (error) console.log(error.stack);
+      console.log("[fail]", "\t", name);
+      if (e) console.log(e.stack);
     }
   }, 500);
 }
